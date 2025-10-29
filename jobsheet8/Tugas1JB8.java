42486cea440cdb549a18655a00eacc0511651a37
daaniys-code <daaniysn@gmail.com>
2025-10-29 10:58:41 +0700

    tugas 1 jobsheet 8

---
+++ jobsheet8/Tugas1JB8.java
@@ -0,0 +1,23 @@
+package jobsheet8;
+
+public class Tugas1JB8 {
+    public static void main(String[] args) {
+        int jumlah;
+
+        for (int n = 1; n<=5; n++){
+            jumlah = 0;
+            for (int i = 1; i <= n; i++){
+                jumlah += i * i;
+            }
+            System.out.print("n = " + n + " => jumlah kuadrat = ");
+
+            for (int j = 1; j <= n; j++){
+                System.out.print(j * j);
+                if (j < n){  
+                    System.out.print(" + ");
+                }
+            }
+            System.out.println(" = " + jumlah);
+        }
+    }
+}
