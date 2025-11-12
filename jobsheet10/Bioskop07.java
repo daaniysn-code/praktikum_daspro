a11c661b40a4335e960edc7c02567c0cf913e802
daaniys-code <daaniysn@gmail.com>
2025-11-12 11:19:04 +0700

    (+) Modifikasi Percobaan 1

--- jobsheet10/Bioskop07.java
+++ jobsheet10/Bioskop07.java
@@ -10,10 +10,49 @@ public class Bioskop07 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        System.out.println();
+
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
+        System.out.println();
+
+        System.out.println(penonton.length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang Baris Ke-" + (i+1) +":" + penonton[i].length);
+        }
+        System.out.println();
+
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang Baris: " + barisPenonton.length);
+        }
+        System.out.println();
+
+        //menggunakan for loop
+        System.out.println("Penonton Pada Baris ke-3: ");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+        System.out.println();
+
+        //menggunakan foreach loop
+        System.out.println("Penonton Pada Baris ke-3: ");
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+        System.out.println();
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton Pada baris ke-" + (i+1) + ":" + String.join(",", penonton[i]));
+        }
     }
 }
